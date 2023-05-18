9c0a62ec304289f82193049ab8370b3e1d0024f7
Sigmestio <juliaskolska@gmail.com>
2023-05-18 10:13:02 +0200

    scriptable i movemnt giga super ugabuga

---
+++ Assets/eggz/EggSriptableObjects.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EggScriptableObject", menuName = "ScriptableObject/EggSriptableObject")]
+
+public class EggSriptableObjects : ScriptableObject
+{
+    public int worth = 1;
+}
