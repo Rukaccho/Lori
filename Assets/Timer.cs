b51bb8baac63804b1980d8d615dc98716adea13e
Sigmestio <juliaskolska@gmail.com>
2023-05-25 19:52:58 +0200

    timerek uwu dziaa

---
+++ Assets/Timer.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
+    public TextMeshProUGUI TimeText;
+    public void Start()
+    {
+        timerIsRunning = true;
+    }
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        TimeText.text = "Time: " + string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
