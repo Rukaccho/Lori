48cbcc916e2e705c14190e4c09d475a02cb3ad09
Sigmestio <juliaskolska@gmail.com>
2023-06-04 19:13:46 +0200

    przenosi score na ekran końcowy
    nie zeruje licznika punktów po ponownym odpaleniu więc zaraz walki ciąg dalszy

--- Assets/GameOverScreen.cs
+++ Assets/GameOverScreen.cs
@@ -7,6 +7,7 @@ public class GameOverScreen : MonoBehaviour
 {
     public Text pointsText;
     public void Setup(int score)
+    
     {
         gameObject.SetActive(true);
         pointsText.text = score.ToString() + " POINTS";
